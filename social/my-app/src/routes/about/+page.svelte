0ae18dc7edb6efd7991b9a72e0e67e9ffcbd0841
Akaljot Sodhi (1000129527) <SODHA2@student.op.ac.nz>
2025-10-08 15:23:35 +1300

    add a route folder for about

---
+++ social/my-app/src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    //gonna put some code here later
+</script>
+
+<h2>About this site</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis sint amet saepe nostrum repudiandae, debitis ab cumque harum quam, consequatur, blanditiis placeat nisi cupiditate distinctio reprehenderit vel qui dicta quisquam!</p>
