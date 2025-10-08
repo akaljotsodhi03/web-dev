276ea7d9b917497c6454eba9552c76ef2e7b8ce7
Akaljot Sodhi (1000129527) <SODHA2@student.op.ac.nz>
2025-10-08 15:35:00 +1300

    add nav bar

---
+++ social/my-app/src/lib/components/navigation.svelte
@@ -0,0 +1,21 @@
+<script>
+    //Navigation Component
+</script>
+
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/comment-section">Comments</a></li>
+    </ul>
+</nav>
+
+<style>
+    ul{
+        list-style-type: none;
+    }
+    
+    li{
+        padding: 1em;
+    }
+</style>
--- social/my-app/src/routes/+layout.svelte
+++ social/my-app/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import Navigation from '$lib/components/navigation.svelte';
 
 	let { children } = $props();
 </script>
@@ -8,4 +9,13 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+
+<h1>My Cool Website!!</h1>
+<Navigation/>
 {@render children?.()}
+
+<style>
+	body{
+		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	}
+</style>
